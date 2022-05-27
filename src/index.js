92d2b2192adfd29c9b4957f83710d3a3b7c09c70
Wayne Chan <wchan.it@outlook.com>
2022-05-27 07:24:02 -0500

    Add actions object for demo purposes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import UserSignupPage from './pages/UserSignupPage';
+import * as apiCalls from './api/apiCalls';
 
-ReactDOM.render(<UserSignupPage />, document.getElementById('root'));
+// for demo purposes
+const actions = {
+  postSignup: apiCalls.signup,
+};
+
+ReactDOM.render(
+  <UserSignupPage actions={actions} />,
+  document.getElementById('root')
+);
