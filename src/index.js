1e8946862e0f238e8466c401a1e1b01c119fbf8b
Wayne Chan <wchan.it@outlook.com>
2022-05-30 07:59:04 -0500

    Add LoginPage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import UserSignupPage from './pages/UserSignupPage';
+import { LoginPage } from './pages/LoginPage';
 import * as apiCalls from './api/apiCalls';
 
 // for demo purposes
@@ -8,7 +9,9 @@ const actions = {
   postSignup: apiCalls.signup,
 };
 
-ReactDOM.render(
-  <UserSignupPage actions={actions} />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <UserSignupPage actions={actions} />,
+//   document.getElementById('root')
+// );
+
+ReactDOM.render(<LoginPage />, document.getElementById('root'));
