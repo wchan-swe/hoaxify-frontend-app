c78b69805b939a59287734f0d9ab11e19aa5062a
Wayne Chan <wchan.it@outlook.com>
2022-05-30 23:52:26 -0500

    Add HomePage and UserPage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  render() {
+    return <div data-testid="homepage">HomePage</div>;
+  }
+}
+
+export default HomePage;
